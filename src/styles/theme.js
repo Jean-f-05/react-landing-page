d867630b90ce18e9288dff5fa795826f6a73b454
Jean-f-05 <jean_f_05@hotmail.com>
2023-01-03 12:04:45 +0000

    feat: added spacing to theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -17,4 +17,14 @@ export const theme = {
   media: {
     upToMedium: '(max-width: 768px)',
   },
+  spacing: {
+    xsmall: '0.8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
 };
