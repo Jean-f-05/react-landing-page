e1458a9f92b841aeffcce3a467a802e370dae6d2
Jean-f-05 <jean_f_05@hotmail.com>
2023-01-24 20:02:45 +0000

    feat: removed App test, Base test & removed templates folder from coverage

--- src/templates/App/App.test.js
+++
@@ -1,8 +0,0 @@
-import { renderTheme } from '../../styles/renderTheme';
-import Home from '.';
-
-describe('Home', () => {
-  it('should render Home', () => {
-    renderTheme(<Home />);
-  });
-});
